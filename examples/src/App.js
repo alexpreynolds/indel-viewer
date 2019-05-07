383f41649988f8d87945ba2416fa3ba6bdd3c1d8
Ubuntu <ubuntu@ip-172-31-25-47.us-east-2.compute.internal>
2019-05-07 22:52:36 +0000

    layout changes

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import './App.css';
-
 import IndelRelativeFrequencyViewer from '../../src';
 
-function App() {
-  return (
-    <div className="demo-parent-container">
-      <div className="demo-viewer-parent-container">
-        <IndelRelativeFrequencyViewer
-          title="Indel Relative Frequency Viewer" />
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewerTitle: "",
+      windowHeight: 0,
+      windowWidth: 0,
+      viewerHeight: 0,
+      viewerWidth: 0,
+    };
+  }
+  
+  componentDidMount() {
+    setTimeout(() => { 
+      this.updateViewportDimensions();
+    }, 100);
+    window.addEventListener("resize", this.updateViewportDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateViewportDimensions);
+  }
+  
+  updateViewportDimensions = () => {
+    let windowInnerHeight = window.innerHeight + "px";
+    let windowInnerWidth = window.innerWidth + "px";
+    let viewerHeight = windowInnerHeight;
+    let viewerWidth = windowInnerWidth;
+    this.setState({
+      windowHeight: windowInnerHeight,
+      windowWidth: windowInnerWidth,
+      viewerHeight: viewerHeight,
+      viewerWidth: viewerWidth,
+      viewerTitle: viewerWidth + " × " + viewerHeight
+    });
+  }
+  
+  render() {
+    return (
+      <div className="demo-parent-container">
+        <div className="demo-viewer-parent-container">
+          <IndelRelativeFrequencyViewer
+            viewerTitle={this.state.viewerTitle}
+            viewerHeight={this.state.viewerHeight}
+            viewerWidth={this.state.viewerWidth} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }  
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -5,29 +5,55 @@ class IndelRelativeFrequencyViewer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      indels :          this.props.indels       || null,
-      title:            this.props.title        || "Default",
-      yAxisLabel :      this.props.yAxisLabel   || "Y-axis label",
-      containerWidth :  this.props.viewerWidth  || 800,
-      containerHeight : this.props.viewerHeight || 600,
-      containerMargin:  this.props.viewerMargin || {
+      indels :                     this.props.indels || null,
+      viewerTitle :                "Default",
+      viewerTitleColor :           "rgba(0, 0, 0, 0.95)",
+      viewerTitleFontWeight :      "600",
+      viewerTitleFontSize :        "1.4em",
+      containerWidth :             800,
+      containerHeight :            600,
+      containerMargin :            {
         top: 15,
         right: 15,
         bottom: 15,
         left: 15
       },
-      containerPadding: this.props.viewerPadding || {
+      containerPadding :           {
         top: 15,
         right: 15,
         bottom: 15,
         left: 15
-      }
+      },
+      containerBackgroundColor :   "rgba(0, 0, 0, 0.05)",
+      yAxisLabel :                 "Y-axis label",
+      xAxisLabel :                 "X-axis label"
     }
   }
   
   render() {
+    let correctedWidth = parseInt(this.props.viewerWidth || this.state.containerWidth) - (this.state.containerMargin.left + this.state.containerMargin.right + this.state.containerPadding.left + this.state.containerPadding.right) + "px";
+    let correctedHeight = parseInt(this.props.viewerHeight || this.state.containerHeight) - (this.state.containerMargin.top + this.state.containerMargin.bottom + this.state.containerPadding.top + this.state.containerPadding.bottom) + "px";
     return (
-      <div id="indel-relative-frequency-viewer">{this.state.title}</div>
+      <div id="irf-container" style={{
+        width: correctedWidth,
+        height: correctedHeight,
+        backgroundColor: this.props.viewerBackgroundColor || this.state.containerBackgroundColor, 
+        marginTop: (this.props.viewerMargin && this.props.viewerMargin.top) || this.state.containerMargin.top, 
+        marginBottom: (this.props.viewerMargin && this.props.viewerMargin.bottom) || this.state.containerMargin.bottom, 
+        marginLeft: (this.props.viewerMargin && this.props.viewerMargin.left) || this.state.containerMargin.left, 
+        marginRight: (this.props.viewerMargin && this.props.viewerMargin.right) || this.state.containerMargin.right, 
+        paddingTop: (this.props.viewerPadding && this.props.viewerPadding.top) || this.state.containerPadding.top, 
+        paddingBottom: (this.props.viewerPadding && this.props.viewerPadding.bottom) || this.state.containerPadding.bottom, 
+        paddingLeft: (this.props.viewerPadding && this.props.viewerPadding.left) || this.state.containerPadding.left, 
+        paddingRight: (this.props.viewerPadding && this.props.viewerPadding.right) || this.state.containerPadding.right}}>
+        <div id="irf-container-title" style={{
+          fontSize: this.props.viewerTitleFontSize || this.state.viewerTitleFontSize,
+          fontWeight: this.props.viewerTitleFontWeight || this.state.viewerTitleFontWeight,
+          color: this.props.viewerTitleColor || this.state.viewerTitleColor,
+        }}>
+          {this.props.viewerTitle || this.state.viewerTitle}
+        </div>
+      </div>
     );
   }
 }
